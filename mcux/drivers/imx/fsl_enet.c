e1a4a9722663b9e3e42ecb1faf46316c2381b317
Xabier Marquiegui <xmarquiegui@ainguraiiot.com>
2021-08-31 10:24:13 +0200

    Check ATCR status both when resetting the timer and when enabling the capture mask

--- mcux/drivers/imx/fsl_enet.c
+++ mcux/drivers/imx/fsl_enet.c
@@ -2800,14 +2800,16 @@ void ENET_Ptp1588Configure(ENET_Type *base, enet_handle_t *handle, enet_ptp_conf
  */
 void ENET_Ptp1588StartTimer(ENET_Type *base, uint32_t ptpClkSrc)
 {
-    /* Restart PTP 1588 timer, master clock. */
-    base->ATCR = ENET_ATCR_RESTART_MASK;
-
     /* Initializes PTP 1588 timer. */
     base->ATINC = ENET_ATINC_INC(ENET_NANOSECOND_ONE_SECOND / ptpClkSrc);
     base->ATPER = ENET_NANOSECOND_ONE_SECOND;
     /* Sets periodical event and the event signal output assertion and Actives PTP 1588 timer.  */
     base->ATCR = ENET_ATCR_PEREN_MASK | ENET_ATCR_PINPER_MASK | ENET_ATCR_EN_MASK;
+    /* Restart PTP 1588 timer, master clock. */
+    base->ATCR |= ENET_ATCR_RESTART_MASK;
+    while ((base->ATCR & ENET_ATCR_RESTART_MASK) == ENET_ATCR_RESTART_MASK) {
+        ;
+    }
 }
 
 /*!
@@ -2820,8 +2822,6 @@ void ENET_Ptp1588StartTimer(ENET_Type *base, uint32_t ptpClkSrc)
  */
 void ENET_Ptp1588GetTimerNoIrqDisable(ENET_Type *base, enet_handle_t *handle, enet_ptp_time_t *ptpTime)
 {
-    uint16_t count = ENET_1588TIME_DELAY_COUNT;
-
     /* Get the current PTP time. */
     ptpTime->second = handle->msTimerSecond;
     /* Get the nanosecond from the master timer. */
@@ -2830,10 +2830,10 @@ void ENET_Ptp1588GetTimerNoIrqDisable(ENET_Type *base, enet_handle_t *handle, en
        It's the requirement when the 1588 clock source is slower
        than the register clock.
     */
-    while (0U != (count--))
-    {
-        __NOP();
+    while ((base->ATCR & ENET_ATCR_CAPTURE_MASK) == ENET_ATCR_CAPTURE_MASK) {
+        ;
     }
+
     /* Get the captured time. */
     ptpTime->nanosecond = base->ATVR;
 }
